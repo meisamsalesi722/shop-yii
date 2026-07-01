8782b142a71e4e127f1279137e05c34b5fb475a3
meisamsalesi722 <salesi52201@gmail.com>
2026-07-01 16:40:07 +0330

    separate frontend and admin styles

---
+++ myproject/assets/FrontendAsset.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @link https://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+declare(strict_types=1);
+
+namespace app\assets;
+
+use yii\bootstrap5\BootstrapAsset;
+use yii\web\AssetBundle;
+use yii\web\View;
+use yii\web\YiiAsset;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class FrontendAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        '/css/bootstrap.css',
+        '/fontawesome/all.css',
+        '/css/owl/owl.carousel.min.css',
+        '/css/owl/owl.theme.default.min.css',
+        '/css/owl/owl.theme.green.min.css',
+        '/css/style.css',
+    ];
+    public $js = [
+        '/js/jquery.min.js',
+        '/js/bootstrap.bundle.min.js',
+        '/js/owl.carousel.min.js',
+        '/js/timer.js',
+        '/js/main.js',
+    ];
+    public $jsOptions = [
+        'position' => View::POS_END,
+    ];
+    public $depends = [
+        YiiAsset::class,
+    ];
+}
--- myproject/views/layouts/main.php
+++ myproject/views/layouts/main.php
@@ -5,35 +5,29 @@ declare(strict_types=1);
 /** @var yii\web\View $this */
 /** @var string $content */
 
-use app\widgets\Alert;
-use yii\bootstrap5\Breadcrumbs;
-use yii\helpers\Html;
+use app\assets\FrontendAsset;
 
-$this->render('_head');
+FrontendAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" class="h-100" data-bs-theme="light">
-<head>
-    <?php $this->head() ?>
-    <title><?= Html::encode($this->title) ?></title>
-</head>
-<body class="d-flex flex-column h-100">
+<html lang="en">
+
+
+ <head>
+     <?php $this->head() ?>
+ </head>
+
+<body>
 <?php $this->beginBody() ?>
 
 <?= $this->render('_header') ?>
 
-<main id="main" class="flex-grow-1" role="main">
-    <div class="container">
-        <?php if (!empty($this->params['breadcrumbs'])): ?>
-            <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
-        <?php endif ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</main>
+       <?= $content ?>
 
 <?= $this->render('_footer') ?>
+
+<div class="black-menu"></div>
+<?= $this->render('_menu') ?>
 
 <?php $this->endBody() ?>
 </body>
